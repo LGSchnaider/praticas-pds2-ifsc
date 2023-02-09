422e48fd8368a59df6f1d70d8bd46f92f8a9a66d
Luiz Gustavo Schnaider <luizgustavoschnaider@gmail.com>
2023-02-09 09:03:36 -0300

    Aula01

---
+++ pratica01/src/control/Conection.java
@@ -0,0 +1,45 @@
+package control;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class Conection {
+	private static Connection conection;
+	private static Conection instancia;
+	private static final String DATABASE = "hr";
+	private static final String USER = "aluno";
+	private static final String PSW = "aluno";
+	
+	private Conection() {}
+	
+	public Conection getInstancia() {
+		if (instancia == null) {
+			instancia = new Conection();
+		}
+		return instancia;
+	}
+	
+	public static Connection link() { //cria conecção com a variavel de memória
+		try {
+			conection = DriverManager.getConnection(
+					"jbbc:mysql://localhost/"
+					+ DATABASE + "?serverTimezone=UTC",
+					USER, PSW);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return conection;
+	}
+	
+	public static boolean closeConection() {
+		try {
+			conection.close();
+		}catch(SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+		
+	}
+	
+}
