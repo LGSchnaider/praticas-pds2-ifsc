27981ccdf642f6a1577479719e81622efdb9793e
Luiz Gustavo Schnaider <luizgustavoschnaider@gmail.com>
2023-03-09 08:54:59 -0300

    v2

--- lista01/src/ex8/Ex8.java
+++ lista01/src/ex8/Ex8.java
@@ -3,20 +3,24 @@ package ex8;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Ex8 {
 	public static void main(String[] args) {
 		try {
 		Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost/"+"?serverTimezone=UTC","root","aluno");
 		
-		Statament stm = conexao.createStatement();
+		Statement stm = conexao.createStatement();
 		String WSQL = "create database Lista01";
-		stm.exetuteQuery(WSQL);
+		stm.executeQuery(WSQL);
+		WSQL = "create table animal";
+		stm.executeQuery(WSQL);
+		
 		
 		
 		conexao.close();
 		}catch(SQLException e) {
-			
+			e.printStackTrace();
 		}
 	}
 }
