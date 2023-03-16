f5496c88ac3ce0d23979975a4a15a20c8260cbf2
Luiz Gustavo Schnaider <luizgustavoschnaider@gmail.com>
2023-03-16 09:02:04 -0300

    v3

--- lista01/src/ex8/Ex8.java
+++ lista01/src/ex8/Ex8.java
@@ -1,25 +1,40 @@
 package ex8;
 
+import java.sql.Array;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Ex8 {
 	public static void main(String[] args) {
 		try {
-		Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost/"+"?serverTimezone=UTC","root","aluno");
-		
-		Statement stm = conexao.createStatement();
-		String WSQL = "create database Lista01";
-		stm.executeQuery(WSQL);
-		WSQL = "create table animal";
-		stm.executeQuery(WSQL);
-		
-		
-		
-		conexao.close();
-		}catch(SQLException e) {
+			Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost/", "root", "aluno");
+
+			Statement stm = conexao.createStatement();
+			String WSQL = "create database if not exists Lista01";
+			stm.executeUpdate(WSQL);
+			
+			WSQL = "use Lista01";
+			stm.executeUpdate(WSQL);
+			
+			WSQL = "CREATE TABLE animal ("
+					+ "  id INT NOT NULL AUTO_INCREMENT,"
+					+ "  classe VARCHAR(45) NOT NULL,"
+					+ "  especie VARCHAR(45) NOT NULL,"
+					+ "  PRIMARY KEY (id))";
+			stm.executeUpdate(WSQL);
+
+			WSQL = "INSERT INTO animal (classe, especie) VALUES ('Mamiféro', 'troglodytes')";
+			stm.executeUpdate(WSQL);
+			
+//			WSQL = "SELECT * FROM animal";
+//			ResultSet a = stm.executeQuery(WSQL);
+
+			
+			conexao.close();
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
